f3248ae11b7a5486730525ae4edab7cd884dd982
klabit87 <keith.labit@gmail.com>
2016-01-05 16:45:12 -0600

    Misc changes/Optimizations

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Mon Dec 21 22:29:32 CST 2015"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 4 23:13:10 CST 2016"
 #define LINUX_COMPILE_BY "klabit"
 #define LINUX_COMPILE_HOST "klabit"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-g03ab933-dirty"
+#define UTS_RELEASE "3.4.0-AIO-kernel"
