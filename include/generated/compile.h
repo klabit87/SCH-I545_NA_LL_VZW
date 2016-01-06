ef9419b79ba38326751a5c5af6b6ce358421dfce
klabit87 <keith.labit@gmail.com>
2016-01-05 19:38:06 -0600

    Add I/O schedulers

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 4 23:13:10 CST 2016"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Jan 5 19:20:48 CST 2016"
 #define LINUX_COMPILE_BY "klabit"
 #define LINUX_COMPILE_HOST "klabit"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
