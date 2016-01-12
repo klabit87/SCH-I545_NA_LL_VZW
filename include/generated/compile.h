a0f4b84741702b58f0f96d6f7b471328d1af0651
klabit87 <keith.labit@gmail.com>
2016-01-11 21:45:19 -0600

    Fix SaberMod 4.9 compile errors

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 31 */
+/* This file is auto generated, version 34 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#31 SMP PREEMPT Sat Jan 9 14:46:46 CST 2016"
+#define UTS_VERSION "#34 SMP PREEMPT Sun Jan 10 23:39:42 CST 2016"
 #define LINUX_COMPILE_BY "klabit"
 #define LINUX_COMPILE_HOST "klabit"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
+#define LINUX_COMPILER "gcc version 4.9.x-sabermod 20150926 (release) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-jfltevzw-AIOkernelR1"
+#define UTS_RELEASE "3.4.0-jfltevzw-AIOkernelLINAROMOD"
