9296a5b6cc01f54ef88ccc7385ffbab6778f3690
klabit87 <keith.labit@gmail.com>
2016-01-14 21:15:12 -0600

    Add Faux Sound

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 34 */
+/* This file is auto generated, version 40 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#34 SMP PREEMPT Sun Jan 10 23:39:42 CST 2016"
+#define UTS_VERSION "#40 SMP PREEMPT Thu Jan 14 12:38:57 CST 2016"
 #define LINUX_COMPILE_BY "klabit"
 #define LINUX_COMPILE_HOST "klabit"
 #define LINUX_COMPILER "gcc version 4.9.x-sabermod 20150926 (release) (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-jfltevzw-AIOkernelLINAROMOD"
+#define UTS_RELEASE "3.4.0-jfltevzw-AIOkernelR2"
